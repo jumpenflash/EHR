a29d5a969ef2dda2fe6a7f9844bdb236c4027917
Jack Hindes <jumpenjackflash@gmail.com>
2017-10-11 15:30:44 -0400

    Added Scheduling service

---
+++ EHR.DAL.Log/Data/Context.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EHR.DAL.Log.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EHR.DAL.Data;
+
+namespace EHR.DAL.Log.Data
+{
+    public class LogContext : BaseContext
+    {
+        public LogContext(DbContextOptions<DbContext> options) : base(options)
+        {
+            LogInitializer.Instance.Initialize(this);
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<EventAttribute> EventAttributes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Event>().ToTable("Event");
+            modelBuilder.Entity<EventType>().ToTable("EventType");
+            modelBuilder.Entity<EventAttribute>().ToTable("EventAttribute");
+        }
+    }
+}
---
+++ EHR.DAL.Log/Data/DBIntializer.cs
@@ -0,0 +1,15 @@
+﻿using EHR.DAL.Data;
+using EHR.DAL.Log.Entities;
+using System;
+using System.Linq;
+
+namespace EHR.DAL.Log.Data
+{
+    public class LogInitializer : DbInitializer<LogContext>
+    {
+        public override void Initialize(LogContext context)
+        {
+            base.Initialize(context);
+        }
+    }
+}
